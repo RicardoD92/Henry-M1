1001d594f9e853f2942bae24a05631dd093ea820
DopinARG <duchiarg@gmail.com>
2022-11-21 16:00:46 -0300

    primer homework M1 terminada 21/11/2022

--- 01-IntroToCS/homework/homework.js
+++ 01-IntroToCS/homework/homework.js
@@ -1,8 +1,27 @@
 'use strict';
 
-function BinarioADecimal(num) {}
+function BinarioADecimal(num) {
+   let sum = 0;
 
-function DecimalABinario(num) {}
+    for (let i = 0; i < num.length; i++) {
+       sum += +num[i] * 2 ** (num.length - 1 - i);
+    }
+    return sum;
+
+
+}
+
+function DecimalABinario(num) {
+   let resultado = [];
+   while (num > 0) {
+       resultado.unshift(num % 2);
+       num = Math.floor(num / 2);
+   }
+
+   return resultado.join('');
+
+
+}
 
 module.exports = {
    BinarioADecimal,
